2d376fe52af8bad207ce72353aadddea61f67ba8
Wahyu Alif Aldika <aldika94wahyu@gmail.com>
2018-12-04 09:38:22 +0700

    Tambah Pengaturan

--- app/Http/Controllers/DataDosenController.php
+++ app/Http/Controllers/DataDosenController.php
@@ -19,7 +19,7 @@ class DataDosenController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth:admin');
     }
 
     public function index()
--- app/Http/Controllers/DataMahasiswaController.php
+++ app/Http/Controllers/DataMahasiswaController.php
@@ -24,9 +24,7 @@ class DataMahasiswaController extends Controller
 
     public function __construct()
     {
-
-        $this->middleware('auth');
-
+        $this->middleware('auth:admin');
     }
 
     public function index()
